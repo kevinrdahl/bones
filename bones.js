fbbd1cf2b67fc925d257092625285c026c888fae
Kevin <kevin.of.dahl@gmail.com>
2014-04-20 13:27:04 -0700

    added images to bones

--- bones.js
+++ bones.js
@@ -7,6 +7,35 @@ var canvasHeight;
 setCanvasSize();
 window.onresize = setCanvasSize();
 
+var imgPrefix = 'http://kevinstuff.net/img/'; //var imgPrefix = 'img/';
+var imageList = ['bone.png'];
+var images = new Object();
+var numLoaded = 0;
+
+function imageLoaded () {
+	var str = this.src.substring(imgPrefix.length);
+	images[str][0] = true;
+	numLoaded++;
+	if (numLoaded == imageList.length) {
+		loadComplete();
+	}
+}
+
+function loadImages() {
+	console.log('loading images');
+	for (var i = 0; i < imageList.length; i++) {
+		var image = new Image();
+		image.onload = imageLoaded;
+		images[imageList[i]] = [false, image];
+		image.src = imgPrefix + imageList[i];
+	}
+}
+
+function loadComplete() {
+	nextTick = new Date().getTime()+33;
+	onTick();
+}
+
 /*======
 CONTROLS
 ======*/
@@ -126,24 +155,24 @@ var origin = [300,300];
 var skeletonAngle = 0;
 
 var bones = {
-	chest:newBone(270,150,null,['head','shoulderleft','shoulderright'],false),
-	head:newBone(270,50,'chest',[],false),
+	chest:newBone(270,150,null,['head','shoulderleft','shoulderright'],false,'bone.png'),
+	head:newBone(270,50,'chest',[],false,'bone.png'),
 	
-	legleft1:newBone(110,80,null,['legleft2'],false),
-	legleft2:newBone(95,80,'legleft1',['footleft'],false),
-	footleft:newBone(180,20,'legleft2',[],false),
+	legleft1:newBone(110,80,null,['legleft2'],false,'bone.png'),
+	legleft2:newBone(95,80,'legleft1',['footleft'],false,'bone.png'),
+	footleft:newBone(180,20,'legleft2',[],false,'bone.png'),
 	
-	legright1:newBone(70,80,null,['legright2'],false),
-	legright2:newBone(85,80,'legright1',['footright'],false),
-	footright:newBone(0,20,'legright2',[],false),
+	legright1:newBone(70,80,null,['legright2'],false,'bone.png'),
+	legright2:newBone(85,80,'legright1',['footright'],false,'bone.png'),
+	footright:newBone(0,20,'legright2',[],false,'bone.png'),
 	
-	shoulderleft:newBone(160,45,'chest',['armleft1'],false),
-	armleft1:newBone(100,70,'shoulderleft',['armleft2'],false),
-	armleft2:newBone(90,65,'armleft1',[],false),
+	shoulderleft:newBone(160,45,'chest',['armleft1'],false,'bone.png'),
+	armleft1:newBone(100,70,'shoulderleft',['armleft2'],false,'bone.png'),
+	armleft2:newBone(90,65,'armleft1',[],false,'bone.png'),
 	
-	shoulderright:newBone(20,45,'chest',['armright1'],false),
-	armright1:newBone(80,70,'shoulderright',['armright2'],false),
-	armright2:newBone(90,65,'armright1',[],false)
+	shoulderright:newBone(20,45,'chest',['armright1'],false,'bone.png'),
+	armright1:newBone(80,70,'shoulderright',['armright2'],false,'bone.png'),
+	armright2:newBone(90,65,'armright1',[],false,'bone.png')
 };
 
 var keyframes = {
@@ -152,9 +181,6 @@ var keyframes = {
 
 var frame = 0;
 var numFrames = 60;
-
-var nextTick = new Date().getTime()+33;
-onTick();
 
 function onTick() {
 	drawFrame();
@@ -173,8 +199,6 @@ function setTick() {
 
 function drawFrame() {
 	context.clearRect(0,0,canvasWidth, canvasHeight);
-	
-	//skeletonAngle = (skeletonAngle + 1) % 360;
 
 	for (name in bones) {
 		if (bones[name].parent == null) {
@@ -185,31 +209,18 @@ function drawFrame() {
 
 	context.strokeStyle = '#000000';
 	
-	for (name in bones) {	
-		var bone = bones[name];
-		
-		context.beginPath()	
-		context.moveTo(bone.coords[0], bone.coords[1]);
-		context.lineTo(bone.endcoords[0], bone.endcoords[1]);
-		context.stroke();
-		
-		context.beginPath()	
-		context.arc(bone.endcoords[0], bone.endcoords[1], 3, 0, 2*Math.PI);
-		context.stroke();
-		
-		if (bone.parent == null) {
-			context.beginPath()	
-			context.arc(bone.coords[0], bone.coords[1], 3, 0, 2*Math.PI);
-			context.stroke();
-		}
+	for (name in bones) {
+		drawBone(bones[name]);
 	}
 }
 
+//poses the specified bone and then recursively poses its children, if it has any
 function poseBones(bone) {
 	var angle = bone.angle+skeletonAngle;
 	if (bone.rigid) {
 		angle += bones[bone.parent].angle;
 	}
+	bone.finalangle = angle;
 	
 	bone.endcoords = LinAlg.pointOffset(bone.coords, angle, bone.len);
 	for (var i = 0; i < bone.children.length; i++) {
@@ -219,8 +230,44 @@ function poseBones(bone) {
 	}
 }
 
-function newBone(angle, len, parent, children, rigid) {
-	return {angle:angle, len:len, parent:parent, children:children, rigid:rigid};
+function drawBone(bone) {
+	//image
+	if (bone.image != null) {
+		if (images[bone.image][0]) {
+			var image = images[bone.image][1];
+			var midpoint = LinAlg.midPoint(bone.coords,bone.endcoords);
+			drawImageRotated(image,midpoint[0],midpoint[1],bone.len,bone.len/3,bone.finalangle);
+		}
+	}
+
+	//wireframe
+	context.beginPath();
+	context.moveTo(bone.coords[0], bone.coords[1]);
+	context.lineTo(bone.endcoords[0], bone.endcoords[1]);
+	context.stroke();
+	
+	context.beginPath()	
+	context.arc(bone.endcoords[0], bone.endcoords[1], 3, 0, 2*Math.PI);
+	context.stroke();
+	
+	if (bone.parent == null) {
+		context.beginPath()	
+		context.arc(bone.coords[0], bone.coords[1], 3, 0, 2*Math.PI);
+		context.stroke();
+	}
+}
+
+function newBone(angle, len, parent, children, rigid, image) {
+	return {angle:angle, len:len, parent:parent, children:children, rigid:rigid, image:image};
+}
+
+//angle is degrees
+function drawImageRotated(image, x, y, w, h, angle) { 
+	context.save();
+	context.translate(x, y);
+	context.rotate(LinAlg.toRadians(angle));
+	context.drawImage(image, -w/2, -h/2, w, h); //x,y,w,h
+	context.restore(); 
 }
 
 function setCanvasSize() {
@@ -232,3 +279,10 @@ function setCanvasSize() {
 
 	console.log('canvas size: ' + canvasWidth + 'x' + canvasHeight);
 }
+
+
+//============
+//	INIT
+//============
+var nextTick;
+loadImages();
